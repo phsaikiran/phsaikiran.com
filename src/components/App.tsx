eefabfe7c6a33e8dd30a4f09bfd550dc3ea5fa3e
P H Sai Kiran <saikiran.2310@gmail.com>
2021-05-16 09:06:19 +0530

    Minor changes, image height, link rel

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -17,16 +17,28 @@ export class App extends React.Component<Props, State> {
                         <img className={"w-64 h-64 rounded-full bg-blu"}
                              src={profile}
                              alt={"profile image"}
+                             height={256}
+                             width={256}
                         />
                     </div>
                     <div className={"text-5xl pt-10 text-blu"}><code>P H Sai Kiran</code></div>
                     <div className={"text-2xl pb-10 text-blu"}><code>Applications Engineer at Oracle</code></div>
                     <div className={"space-x-5"}>
-                        <a href={"https://www.linkedin.com/in/phsaikiran/"} target={"_blank"}>
-                            <img className={"w-10 h-10 inline-block"} src={linkedin} alt={"linkedin logo"}/>
+                        <a href={"https://www.linkedin.com/in/phsaikiran/"} target={"_blank"} rel={"noreferrer"}>
+                            <img className={"w-10 h-10 inline-block"}
+                                 src={linkedin}
+                                 alt={"linkedin logo"}
+                                 height={40}
+                                 width={40}
+                            />
                         </a>
-                        <a href={"https://github.com/phsaikiran"} target={"_blank"}>
-                            <img className={"w-10 h-10 inline-block"} src={github} alt={"github logo"}/>
+                        <a href={"https://github.com/phsaikiran"} target={"_blank"} rel={"noreferrer"}>
+                            <img className={"w-10 h-10 inline-block"}
+                                 src={github}
+                                 alt={"github logo"}
+                                 height={40}
+                                 width={40}
+                            />
                         </a>
                     </div>
                 </div>
