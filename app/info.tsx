708f4e479aa7d44bf36cd3d71feb0d554a2016e1
phsaikiran <saikiran.2310@gmail.com>
2023-08-31 17:27:23 -0500

    Updated about

--- app/info.tsx
+++ app/info.tsx
@@ -7,11 +7,12 @@ export const about = () => {
     <>
       <b>Hey, {"I'm"} Sai!</b><br/>
       {"I'm"} a passionate and dedicated computer science professional with a strong foundation in intelligent systems
-      and
-      data science.
-      My journey in the world of technology has been driven by a relentless curiosity to explore and innovate.
-      With a {"Master's"} in Computer Science from The University of Texas at Dallas,
-      {"I've"} cultivated a diverse skill set that spans the realms of software development, machine learning, and more.
+      and data science. I have a proven track record of delivering impactful solutions through hands-on experience in
+      software development and data analysis, complemented by a proactive approach to innovation and continuous
+      improvement.<br/>
+      My journey in the world of technology has been driven by a relentless curiosity to explore and
+      innovate. With a {"Master's"} in Computer Science from The University of Texas at Dallas, {"I've"} cultivated
+      a diverse skill set that spans the realms of software development, machine learning, and more.
     </>
   );
 };
