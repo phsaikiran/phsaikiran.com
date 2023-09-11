7b5355cb05d550ad42cdc02eeb27a82d51fd687b
phsaikiran <saikiran.2310@gmail.com>
2023-09-11 11:48:36 -0500

    Updated about

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -15,39 +15,8 @@ export default function AboutPage() {
         <p>
           {"I'm"} currently seeking new and exciting opportunities that allow me to apply my expertise and
           passion in the field of computer science and data science. With a strong foundation in
-          intelligent systems, data analysis, and software development,
-          {"I'm"} eager to contribute to innovative projects and collaborate with dynamic teams.
-        </p>
-        <hr/>
-        <p>
-          During my time as an <b>Applications Engineer at Oracle</b>, I had the privilege
-          to work on a variety of projects, ranging from enhancing existing Java/Spring applications to
-          architecting and developing full-stack solutions from scratch. Leveraging my proficiency in
-          technologies like Java, Oracle JET, Docker, Kubernetes, and Jenkins, I championed containerization
-          and streamlined continuous integration and deployment processes.
-        </p>
-        <p>
-          My commitment to staying at the forefront of technological advancements led me to my role as a
-          <b> Research Assistant</b> at the Institute for Data Analytics at UT Dallas. Here, {"I've"} harnessed my
-          analytical prowess to conduct data analysis, develop Java applications, and contribute to
-          migrating legacy systems, all while keeping a keen eye on UI test cases and performance
-          optimization.
-        </p>
-        <p>
-          My academic journey has been punctuated by noteworthy projects, from a movie recommendation system
-          driven by machine learning to object detection for piano keys using computer vision. {"I've"} also
-          delved into the intricacies of database design for an e-commerce platform and tackled challenges in
-          operating systems with a lottery scheduler for xv6.
-        </p>
-        <p className="mb-8">
-          Beyond technical prowess, {"I've"} demonstrated leadership as the Convenor (Head) of the Institution of
-          Engineering and Technology at NITK, fostering an environment of collaboration and knowledge sharing.
-
-          Having been recognized twice with the {"'OAL CRM Org'"} Award at Oracle for my innovative and agile
-          approach, {"I'm"} steadfast in my pursuit of excellence, continuously pushing boundaries and embracing
-          challenges. {"I'm"} excited to bring my expertise, enthusiasm, and dedication to contribute meaningfully
-          to new opportunities and technological frontiers. {"Let's"} connect and explore how we can create a
-          transformative impact together!
+          intelligent systems, data analysis, and software development, {"I'm"} eager to contribute to innovative
+          projects and collaborate with dynamic teams.
         </p>
         <div className="flex flex-col gap-2 md:flex-row md:gap-2">
           <a
@@ -87,6 +56,64 @@ export default function AboutPage() {
             <ArrowIcon/>
           </a>
         </div>
+        <hr/>
+
+        <h3 className="font-bold text-xl font-serif">Education</h3>
+        <p>
+          I hold a Master of Science in Computer Science from The University of Texas at Dallas, where I maintained an
+          impressive GPA of 3.93/4. My academic journey has equipped me with a deep understanding of cutting-edge
+          technologies in the realm of computer science. Some of the courses {"I've"} excelled in include Machine
+          Learning, Statistical Methods, Computer Vision, Natural Language Processing, and Big Data Management and
+          Analysis.
+        </p>
+        <hr/>
+
+        <h3 className="font-bold text-xl font-serif">Technical Expertise</h3>
+        <p>
+          I possess a diverse set of technical skills, including proficiency in both back-end and front-end development.
+          My toolkit includes Java, Node.js, C++, Python, PHP, Rust, and GoLang for back-end development. On the
+          front-end, I excel in React.js, Oracle JET, HTML5, CSS3, JavaScript, TypeScript, and jQuery.
+        </p>
+        <p>
+          In the realm of databases, {"I'm"} well-versed in MySQL, Oracle SQL, DynamoDB, and MongoDB. {"I'm"} also
+          experienced in various tools and technologies such as Apache Kafka, Docker, Kubernetes, Helm charts, AWS
+          Lambda, Git, Jenkins, and GitHub Actions. My training includes certifications in Neural Networks, Deep
+          Learning, and Agile SAFe 4 Practitioner.
+        </p>
+        <hr/>
+
+        <h3 className="font-bold text-xl font-serif">Professional Experience</h3>
+        <p>
+          As a Research Assistant at The University of Texas at Dallas, {"I've"} actively contributed to enhancing
+          security in full-stack Java applications, resulting in the resolution of 100% of reported
+          vulnerabilities. {"I've"} also successfully migrated legacy applications to Java LTS and Maven, achieving
+          improved security and a 20% performance boost.
+        </p>
+        <p>
+          During my tenure as a Software Developer at Oracle, Bangalore, I played a pivotal role in developing
+          performance-enhancing features for Java/Spring applications, resulting in a 30% increase in lead processing
+          speed. I also designed real-time log capturing applications that boosted WebLogic team productivity by 90%.
+          Leveraging Apache Kafka, I streamlined marketing data processing, reducing lead wait times by 25%.
+          Additionally, I implemented Docker and Kubernetes for efficient CI/CD, leading to a 40% reduction in
+          deployment times.
+        </p>
+        <hr/>
+
+        <h3 className="font-bold text-xl font-serif">Academic Projects</h3>
+        <p>
+          I have a strong background in practical project work, including developing a Movie Recommendation System using
+          Machine Learning, implementing Piano Keys Object Detection for a piano tutor application using Computer
+          Vision, and creating a full-stack E-commerce platform with a focus on Database Design. {"I've"} also worked on
+          projects related to operating systems and robotics.
+        </p>
+        <hr/>
+
+        <h3 className="font-bold text-xl font-serif">Student Organizations</h3>
+        <p className="mb-8">
+          As the Convenor of the Institution of Engineering and Technology (IET) at NITK, I led strategic event
+          planning, recruitment, and project execution, resulting in a 25% increase in memberships. I fostered
+          cross-collaboration among presidents and IET leaders, enhancing organizational synergy and impact.
+        </p>
       </div>
     </section>
   );
