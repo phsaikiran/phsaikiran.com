981acce54d5b319d98d832ee401b310fa995393b
phsaikiran <saikiran.2310@gmail.com>
2023-09-11 11:16:44 -0500

    Updated resume

--- app/info.tsx
+++ app/info.tsx
@@ -6,13 +6,9 @@ export const about = () => {
   return (
     <>
       <b>Hey, {"I'm"} Sai!</b><br/>
-      {"I'm"} a passionate and dedicated computer science professional with a strong foundation in intelligent systems
-      and data science. I have a proven track record of delivering impactful solutions through hands-on experience in
-      software development and data analysis, complemented by a proactive approach to innovation and continuous
-      improvement.<br/>
-      My journey in the world of technology has been driven by a relentless curiosity to explore and
-      innovate. With a {"Master's"} in Computer Science from The University of Texas at Dallas, {"I've"} cultivated
-      a diverse skill set that spans the realms of software development, machine learning, and more.
+      An experienced computer scientist with over 4 years of expertise in full-stack development, specializing in
+      intelligent systems and data science. I have demonstrated success in impactful software solutions and enhancing
+      application security.<br/>
     </>
   );
 };
