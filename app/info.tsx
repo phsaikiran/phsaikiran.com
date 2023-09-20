b260556af0b02f09fd58d740d4e8f2ec13cde5e5
phsaikiran <saikiran.2310@gmail.com>
2023-09-19 20:20:52 -0500

    Updated info

--- app/info.tsx
+++ app/info.tsx
@@ -6,7 +6,7 @@ export const about = () => {
   return (
     <>
       <b>Hey, {"I'm"} Sai!</b><br/>
-      An experienced computer scientist with over 4 years of expertise in full-stack development, specializing in
+      An experienced computer scientist with over 3 years of expertise in full-stack development, specializing in
       intelligent systems and data science. I have demonstrated success in impactful software solutions and enhancing
       application security, driven by innovation and continuous improvement.<br/>
     </>
