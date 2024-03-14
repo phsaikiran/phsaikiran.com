434d0b011b91225e7e6df00f37a0892b0a5b3b4e
phsaikiran <saikiran.2310@gmail.com>
2024-03-14 16:21:58 -0500

    Updated resume

--- app/info.tsx
+++ app/info.tsx
@@ -6,7 +6,7 @@ export const about = () => {
   return (
     <>
       <b>Hey, {"I'm"} Sai!</b><br/>
-      I am an experienced software developer with over 3 years of expertise in Java full-stack development, specializing
+      I am an experienced software developer with over 4 years of expertise in Java full-stack development, specializing
       in intelligent systems and data science. I have demonstrated success in impactful software solutions and enhancing
       application security, driven by innovation and continuous improvement.<br/>
     </>
