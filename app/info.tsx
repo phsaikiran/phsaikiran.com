52664345dfb04d640127eac37b8eb3a8de26da76
phsaikiran <saikiran.2310@gmail.com>
2024-04-04 17:46:50 -0500

    Updated bio and resume

--- app/info.tsx
+++ app/info.tsx
@@ -6,9 +6,10 @@ export const about = () => {
   return (
     <>
       <b>Hey, {"I'm"} Sai!</b><br/>
-      I am an experienced software developer with over 4 years of expertise in full-stack development, specializing in
-      Java, React.js, SQL, Kafka, and Kubernetes. I am proficient in implementing scalable solutions and optimizing
-      performance through innovation.<br/>
+      I am an experienced software developer with 3+ years of expertise in full-stack development, specializing in Java,
+      React.js, AWS, SQL, Kafka, and Kubernetes. I am proficient in implementing scalable solutions through Kubernetes
+      and microservices as well as optimizing performance through caching, leading to a {"<"}100ms response times for
+      critical REST APIs<br/>
     </>
   );
 };
